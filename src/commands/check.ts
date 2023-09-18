2f1b7c1fc72412c767a0dacc44ca30fff5d70cfc
VincentGonnet <vincent.gonnet08@gmail.com>
2023-09-18 12:14:03 +0200

    refractor: externalized sales check process in a module

--- src/commands/check.ts
+++ src/commands/check.ts
@@ -1,5 +1,6 @@
 import Database from 'bun:sqlite';
 import{ Client, SlashCommandBuilder, ChatInputCommandInteraction, EmbedBuilder } from 'discord.js';
+import { checkSales } from '../utils/check-sales';
 
 export default {
     data: new SlashCommandBuilder()
@@ -24,94 +25,7 @@ export default {
         const homeWorld = preferences[0].homeworld;
         const language = preferences[0].language;
 
-        const undercuts = [];
-        let solds = [];
-
-        const treatedListings = [];
-        for (const sale of sales) {
-            const itemId : number = sale.item_id;
-            const retainerName : string = sale.retainer;
-
-            const apiMarketResponse : any = await fetch(`https://universalis.app/api/v2/${homeWorld}/${itemId}`);
-            const jsonMarketResponse : any = await apiMarketResponse.json();
-
-            const listings : any = jsonMarketResponse.listings;
-
-            let bought : boolean = true;
-            for (const listing of listings) {
-                if (listing.retainerName === retainerName && !treatedListings.includes(listing.listingID)) {
-                    treatedListings.push(listing.listingID);
-                    bought = false;
-                    break;
-                }
-            }
-
-            if (bought) {
-                solds.push(itemId);
-            } else if (listings.length > 0 && listings[0].retainerName != retainerName) {
-                undercuts.push(itemId);
-            }
-        }
-
-        const soldsItems = []
-        for (const sold of solds) {
-            const apiItemResponse : any = await fetch(`https://xivapi.com/item/${sold}`);
-            const jsonItemResponse : any = await apiItemResponse.json();
-
-            switch (language) {
-                case 'fr':
-                    soldsItems.push(jsonItemResponse.Name_fr);
-                    break;
-                case 'de':
-                    soldsItems.push(jsonItemResponse.Name_de);
-                    break;
-                case 'ja':
-                    soldsItems.push(jsonItemResponse.Name_ja);
-                    break;
-                default:
-                    soldsItems.push(jsonItemResponse.Name);
-                    break;
-            }
-            await db.query(`DELETE FROM sales WHERE rowid in (SELECT rowid FROM sales WHERE user_id = $1 AND item_id = $2 LIMIT 1)`).run({$1: userId, $2: sold});
-        }
-
-        const items = [];
-        for (const undercut of undercuts) {
-            const apiItemResponse : any = await fetch(`https://xivapi.com/item/${undercut}`);
-            const jsonItemResponse : any = await apiItemResponse.json();
-
-            switch (language) {
-                case 'fr':
-                    items.push(jsonItemResponse.Name_fr);
-                    break;
-                case 'de':
-                    items.push(jsonItemResponse.Name_de);
-                    break;
-                case 'ja':
-                    items.push(jsonItemResponse.Name_ja);
-                    break;
-                default:
-                    items.push(jsonItemResponse.Name);
-                    break;
-            }
-        }
-        
-        const responseEmbed = new EmbedBuilder()
-            .setTitle('Undercut checkup')
-            .setFooter({text: `🛎️ ${homeServer} - ${homeWorld}`})
-            .setColor('#e98640');
-        
-        if (undercuts.length > 0) {
-            const itemsList = items.join('\n▫️');
-            responseEmbed.addFields({name: 'You got undercut for the following sales', value: `▫️${itemsList}`});
-        } else {
-            responseEmbed.addFields({name: 'You didn\'t get undercut for any of your sales', value: '🎉'});
-        }
-
-        if (solds.length > 0) {
-            const soldsList = soldsItems.join('\n▫️');
-            responseEmbed.addFields({name: 'The following items have been sold', value: `▫️${soldsList}`});
-        }
+        const responseEmbed = await checkSales(db, sales, homeServer, homeWorld, language, userId);
 
         await interaction.reply({embeds: [responseEmbed], ephemeral: true});
     },
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,9 +1,60 @@
-import { Client, Events } from 'discord.js';
+import Database from 'bun:sqlite';
+import { ActivityType, Client, Events } from 'discord.js';
+import { checkSales } from '../utils/check-sales';
 
 export default {
 	name: Events.ClientReady,
 	once: true,
-	execute(client: Client) {
+	async execute(client: Client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+
+		client.user.setPresence({
+			status: 'online',
+			activities: [{
+				name: 'FFXIV Market Board',
+				type: ActivityType.Watching
+			}],
+		});
+
+		const db : Database = client.db;
+		const sales : any = db.query(`SELECT * FROM sales`).all();
+
+		interface Sale {
+			user_id: string,
+			retainer: string,
+			item_id: number,
+			automatic_checks: number
+		}
+
+		if (sales.length === 0) return;
+
+		const salesByUser : Sale[][] = [];
+
+		for (const sale of sales) {
+			const userId : string = sale.user_id;
+			const userSales : Sale[] = salesByUser.find(sales => sales[0].user_id === userId);
+
+			if (userSales) {
+				userSales.push(sale);
+			} else {
+				salesByUser.push([sale]);
+			}
+		}
+
+		for (const userSales of salesByUser) {
+			const userId : string = userSales[0].user_id;
+			const preferences : any = await db.query(`SELECT * FROM users WHERE id = $1`).all({$1: userId});
+
+			if (preferences.length === 0) continue;
+
+			const homeServer = preferences[0].datacenter;
+			const homeWorld = preferences[0].homeworld;
+			const language = preferences[0].language;
+
+			const responseEmbed = await checkSales(db, userSales, homeServer, homeWorld, language, userId);
+
+			const user = await client.users.fetch(userId);
+			await user.send({embeds: [responseEmbed]});
+		}
 	},
 };
